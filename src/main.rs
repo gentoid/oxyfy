6512c8acc34cfedc935f63858bbf2c19597475e2
Viktor Lazarev <viktor.lazarev@p3-group.com>
2019-08-07 15:38:23 +0200

    Use example code to test nannou

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,22 @@
-extern crate libpulse_binding as pulse;
-extern crate libpulse_simple_binding as psimple;
-extern crate minimp3;
+// use {std::{env, fs::File}, nannou::prelude::*};
 
-use minimp3::{Decoder, Error, Frame};
-use std::env;
-use std::fs::File;
-
-use psimple::Simple;
-use pulse::stream::Direction;
+use nannou::prelude::*;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let filename = &args[1];
+    // let args: Vec<String> = env::args().collect();
+    // let filename = &args[1];
 
-    let spec = pulse::sample::Spec {
-        format: pulse::sample::SAMPLE_S16NE,
-        channels: 2,
-        rate: 44100,
-    };
-    assert!(spec.is_valid());
+    nannou::app(model).update(update).simple_window(view).run();
+}
 
-    let stream = Simple::new(
-        None,                 // Use the default server
-        "Oxyfy music player", // Our application’s name
-        Direction::Playback,  // We want a playback stream
-        None,                 // Use the default device
-        "Music",              // Description of our stream
-        &spec,                // Our sample format
-        None,                 // Use default channel map
-        None,                 // Use default buffering attributes
-    )
-    .unwrap();
+struct Model {}
 
-    let mut decoder = Decoder::new(File::open(filename).unwrap());
+fn model(_app: &App) -> Model {
+    Model {}
+}
 
-    loop {
-        match decoder.next_frame() {
-            Ok(Frame { data, .. }) => {
-                let splitten: Vec<[u8; 2]> = data.into_iter().map(|el| el.to_ne_bytes()).collect();
-                let buffer: Vec<u8> = splitten.iter().flatten().map(|el| *el).collect();
-                stream.write(&buffer).unwrap();
-            }
-            Err(Error::Eof) => break,
-            Err(err) => panic!("{:?}", err),
-        }
-    }
+fn update(_app: &App, _model: &mut Model, _update: Update) {}
+
+fn view(_app: &App, _model: &Model, frame: &Frame) {
+    frame.clear(PURPLE);
 }
