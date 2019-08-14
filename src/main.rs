19c4ba713ad9fbbd8c4c1400b96dc63c9ec3cf70
Viktor Lazarev <taurus101v@gmail.com>
2019-08-14 17:42:18 +0200

    Remove commented out code

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
-// use {std::{env, fs::File}, nannou::prelude::*};
-
 use nannou::prelude::*;
 
 fn main() {
-    // let args: Vec<String> = env::args().collect();
-    // let filename = &args[1];
-
     nannou::app(model).update(update).simple_window(view).run();
 }
 
